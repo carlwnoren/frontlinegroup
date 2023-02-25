44619a1608c471479553ecde69f926fdaa57aa6f
Tim Carlson <timmothy.carlson@gmail.com>
2023-02-24 19:46:56 -0600

    Add example code for ListView.

--- quevie/src/main/java/com/frontline/quevie/userinterface/SearchResultsController.java
+++ quevie/src/main/java/com/frontline/quevie/userinterface/SearchResultsController.java
@@ -3,20 +3,43 @@ package com.frontline.quevie.userinterface;
 import com.frontline.quevie.QuevieApplication;
 import com.frontline.quevie.data.Movie;
 import com.frontline.quevie.data.MovieDatabase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Cell;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class SearchResultsController {
-    private List<Movie> searchResults = MovieDatabase.getInstance().getSearchResults();
+    private ObservableList<Movie> searchResults = FXCollections.observableArrayList(MovieDatabase.getInstance().getSearchResults());
     @FXML
     private Button backButton;
+
+    @FXML
+    private ListView results;
+
+    @FXML
+    protected void initialize() {
+        results.setItems(searchResults);
+        results.setCellFactory(new Callback<ListView<Movie>, ListCell<Movie>>() {
+            @Override public ListCell<Movie> call(ListView<Movie> list) {
+                return new MovieFormatCell();
+            }
+        });
+    }
+
 
     @FXML
     protected void onBackButtonClick(ActionEvent actionEvent) throws IOException {
@@ -31,5 +54,18 @@ public class SearchResultsController {
         Scene scene = new Scene(fxmlLoader.load());
 
         ((Stage)backButton.getScene().getWindow()).setScene(scene);
+    }
+
+
+}
+
+class MovieFormatCell extends ListCell<Movie> {
+
+    public MovieFormatCell() {    }
+
+    @Override protected void updateItem(Movie item, boolean empty) {
+        // calling super here is very important - don't skip this!
+        super.updateItem(item, empty);
+        setText(item == null ? "" : item.getTitle() + " (" + item.getYearMade() + ")");
     }
 }
