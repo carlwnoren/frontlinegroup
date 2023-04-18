38f891d9f24517bb3990f152d9221b3f189c1d69
codywi <64983732+codywi@users.noreply.github.com>
2023-04-17 21:32:05 -0500

    more movies, for the memes

--- quevie/src/main/java/com/frontline/quevie/QuevieApplication.java
+++ quevie/src/main/java/com/frontline/quevie/QuevieApplication.java
@@ -40,12 +40,25 @@ public class QuevieApplication extends Application {
         cast.add("Nicholas Hoult");
         database.addMovie("Mad Max: Fury Road", "2015", "Action", "George Miller", cast);
 
-        MovieDatabase database2 = MovieDatabase.getInstance();
+//        MovieDatabase database2 = MovieDatabase.getInstance();
         ArrayList<String> cast2 = new ArrayList<String>();
         cast2.add("Marlon Brando");
         cast2.add("Al Pacino");
         cast2.add("James Caan");
         database.addMovie("The Godfather", "1972", "Crime/Drama", "Warren Clymer", cast2);
+
+        ArrayList<String> cast3 = new ArrayList<>();
+        cast3.add("Quentin Tarantino");
+        cast3.add("Uma Thurman");
+        cast3.add("Bruce Willis");
+        database.addMovie("Pulp Fiction","1994","Crime/Drama","Quentin Tarantino",cast3);
+
+        ArrayList<String> cast4 = new ArrayList<>();
+        cast4.add("Will Arnett");
+        cast4.add("Patton Oswalt");
+        cast4.add("Lou Romano");
+        database.addMovie("Ratatouille","2007","Family/Comedy","Brad Bird",cast4);
+
         launch();
     }
 }
